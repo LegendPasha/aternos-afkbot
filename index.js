9b972f011ae8de2db0a331ac6eefac320a25ec43
LegendPasha <82825726+LegendPasha@users.noreply.github.com>
2021-11-18 12:49:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ var moving = 0;
 var connected = 0;
 var actions = [ 'forward', 'back', 'left', 'right']
 var lastaction;
-var pi = 3.14159;
+var pi = 3.14159;                                                      
 var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
-var host = data["ip"];
-var port = data["port"];
-var username = data["name"]
+var host = data["BestHitNW.aternos.me"];
+var port = data["19877"];
+var username = data["Bot1"]
 var bot = mineflayer.createBot({
   host: host,
   port: port,
